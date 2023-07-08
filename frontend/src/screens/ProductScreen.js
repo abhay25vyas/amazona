f90cf7eea7a9d4eb35bd32c45f1c272c5fa1e6ee
abhay.vyas25 <abhay.vyas25@gmail.com>
2023-07-08 19:53:58 +0530

    Setup Basic Front and backend

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ProductScreen = (props) => {
+  const param = useParams();
+  const { slug } = param;
+
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+};
+
+export default ProductScreen;
